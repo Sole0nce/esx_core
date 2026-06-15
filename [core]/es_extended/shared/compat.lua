d3868429ed02461809702712a648616fc2882852
SNAILX <ahmedits45@gmail.com>
2026-06-15 02:08:30 +0100

    Moved esx.table from the core to esx_lib

--- [core]/es_extended/shared/compat.lua
+++ [core]/es_extended/shared/compat.lua
@@ -1,1 +1,17 @@
 --All shared functions outsourced from the Core to the lib will be stored here for compatability, e.g:
+ESX.Table.SizeOf = xLib.table.size
+ESX.Table.Set = xLib.table.set
+ESX.Table.IndexOf = xLib.table.indexOf
+ESX.Table.LastIndexOf = xLib.table.lastIndexOf
+ESX.Table.Find = xLib.table.find
+ESX.Table.FindIndex = xLib.table.findIndex
+ESX.Table.Filter = xLib.table.filter
+ESX.Table.Map = xLib.table.map
+ESX.Table.Reverse = xLib.table.reverse
+ESX.Table.Clone = xLib.table.clone
+ESX.Table.Concat = xLib.table.concat
+ESX.Table.Join = xLib.table.join
+ESX.Table.TableContains = xLib.table.contains
+ESX.Table.Sort = xLib.table.sort
+ESX.Table.ToArray = xLib.table.toArray
+ESX.Table.Wipe = xLib.table.wipe
