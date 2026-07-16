9a4c492b51e09511d3cd1a18cc8132921ce86dcf
_Not_ <notequalnullde@gmail.com>
2026-07-16 00:55:38 -0500

    Add initialization for ESX.Table in compat.lua

--- [core]/es_extended/shared/compat.lua
+++ [core]/es_extended/shared/compat.lua
@@ -1,5 +1,7 @@
 --All shared functions outsourced from the Core to the lib will be stored here for compatability, e.g:
 
+ESX.Table = {}
+
 ESX.SetTimeout = xLib.timeout.setTimeout
 ESX.ClearTimeout = xLib.timeout.clearTimeout
 ESX.Await = xLib.waitFor
